b6d6db20a8ce0988e57b57f0bc2c6cc4cac81d78
akfrescki <karolinefrescki@gmail.com>
2026-02-24 16:05:20 +0100

    contact form added

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,6 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :contact_messages, only: :create
-  resources :messages, only: [:create]
   # Defines the root path route ("/")
   # root "posts#index"
 end
